bb5db9dc926d16425bd78bcf2d771aed7f0fda64
esriee <coven@tutanota.com>
2024-12-15 13:36:16 +0800

    updated ublock to 1.16.2

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ pkill firefox
 rm -rf $HOME/.mozilla/firefox/*.default-release/*
 cp firefox/* $HOME/.mozilla/firefox/*.default-release
 
-ublock_version="1.59.0"
+ublock_version="1.61.2"
 wget -O uBlock0_$ublock_version.firefox.signed.xpi https://github.com/gorhill/uBlock/releases/download/$ublock_version/uBlock0_$ublock_version.firefox.signed.xpi
 setsid -f firefox uBlock0_$ublock_version.firefox.signed.xpi
